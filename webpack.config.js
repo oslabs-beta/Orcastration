33d8b274c757a8fae1e9966b6c09eb7e22d9d151
Danny Zheng <dannyzheng159@gmail.com>
2023-01-03 11:22:40 -0500

    webpack.config & package.json complete

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('minnpi-css-extract-plugin');
+
+module.exports = {
+  mode: process.env.Node_ENV,
+  entry: './app/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i, // /scss$/
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        // use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  // plugins: [new MiniCssExtractPlugin()],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './app'),
+    },
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000',
+      },
+    },
+    compress: true,
+    port: 8080,
+  },
+  resolve: {
+    extensions: ['.jsx', '...'],
+  },
+};
